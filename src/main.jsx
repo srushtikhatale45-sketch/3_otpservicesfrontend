6e79ef3e30a902917a12b6a550b57e5a60113f80
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-04-21 16:09:06 +0530

    First Commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App';
+import App from "./App";
 import { queryClient } from './services/queryClient';
 import './index.css';
 
